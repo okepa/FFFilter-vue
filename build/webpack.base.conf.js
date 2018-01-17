0ec52d27454c53c0c0a74a83c82f2deb7aecf197
Olek <okepa@yahoo.co.uk>
2018-01-17 18:04:28 +0000

    Added npm package

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -61,7 +61,17 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
       }
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin("styles.css"),
+  ]
 }
